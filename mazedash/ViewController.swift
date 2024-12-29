2d8205e83b4831d1b85f58ab162e3ad55eed2ed5
Clementgann <clement@oceantech.asia>
2024-12-29 16:36:29 +0800

    Initial Commit

---
+++ mazedash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mazedash
+//
+//  Created by Clement Gan on 29/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
